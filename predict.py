519534959e1b959e739afb073014761b338761b6
codersayan0 <sayanmandal7212@gmail.com>
2026-03-25 09:02:22 +0530

    Fix model crash issue

--- predict.py
+++ predict.py
@@ -1,22 +1,35 @@
 import numpy as np
+import os
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
 MODEL_PATH = "model/trained_model.h5"
 
-model = load_model(MODEL_PATH)
+model = None
 
-# Warm-up
-model.predict(np.zeros((1,224,224,3)), verbose=0)
+# SAFE LOAD
+try:
+    print("Checking model path:", os.path.exists(MODEL_PATH))
+    model = load_model(MODEL_PATH)
+    print("Model loaded successfully")
+
+    # Warm-up
+    model.predict(np.zeros((1, 224, 224, 3)), verbose=0)
+
+except Exception as e:
+    print("Model load failed:", e)
 
 
 def predict_image(img_path):
+
+    if model is None:
+        return {"error": "Model not loaded"}
+
     img = image.load_img(img_path, target_size=(224, 224))
     img = image.img_to_array(img) / 255.0
     img = np.expand_dims(img, axis=0)
 
     pred = model.predict(img, verbose=0)[0][0]
-
     pred = float(pred)
 
     if pred > 0.5:
